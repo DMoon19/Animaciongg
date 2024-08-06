cdcaf6b7cb5e3bb4b8222451cf4ded6603e5a45f
David García <davidgarciasanchez09@gmail.com>
2024-08-06 18:59:49 -0500

    Mov en funcion a la camara

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -7,6 +7,7 @@ using CallbackContext = UnityEngine.InputSystem.InputAction.CallbackContext;
 public class CharacterMovement : MonoBehaviour
 {
     [SerializeField] private Animator anim;
+    [SerializeField] private Transform cameraTransform;
     [SerializeField] private VectorDampener motionVector = new VectorDampener(true);
 
 
@@ -41,5 +42,15 @@ public class CharacterMovement : MonoBehaviour
         Vector2 direction = motionVector.CurrentValue;
         anim.SetFloat(VelXId, direction.x);
         anim.SetFloat(VelYId, direction.y);
+    }
+    private void OnAnimatorMove()
+    {
+        float interpolator = Mathf.Abs(Vector3.Dot(cameraTransform.forward, transform.up));
+        Vector3 forward  = Vector3.Lerp(cameraTransform.forward, cameraTransform.up, interpolator);
+        Vector3 projected = Vector3.ProjectOnPlane(forward, transform.up);
+        Quaternion rotation = Quaternion.LookRotation(projected, transform.up);
+        anim.rootRotation = Quaternion.Slerp(anim.rootRotation, rotation, motionVector.CurrentValue.magnitude);
+        anim.ApplyBuiltinRootMotion();
+
     }
 }
